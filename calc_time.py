57001d28474c3264c63bd7d0cda0b14beae4c9fa
MashaSamoylova <mashanedyak@gmail.com>
2018-07-28 16:18:32 +0700

    [*]calc

--- calc_time.py
+++ calc_time.py
@@ -3,8 +3,7 @@ import datetime
 def calc_start_time(time: int, unit):
     now = datetime.datetime.now()
     if unit == "minutes":
-        return now - datetime.timedelta(hours=1, minutes=23))
+        return now - datetime.timedelta(hours=0, minutes=time)
+    elif unit == "hours":
+        return now - datetime.timedelta(hours=time)
 
-
-if __name__=="__main__":
-    calc_start_time(5, "minutes")
