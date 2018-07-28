119eb57aa0ab692a620b66e7549224a4548ab022
MashaSamoylova <mashanedyak@gmail.com>
2018-07-28 19:10:37 +0700

    asdf

--- calc_time.py
+++ calc_time.py
@@ -12,13 +12,13 @@ def calc_start_time(time_s: int, unit):
 def calc_free_time(user_id, session_id):
     data = select_all_data(user_id, session_id)
     start_time = data['start_time']
-    free_time = data['free_time']
+    free_time = data['free_period']
     return free_time - (int(time.time()) - start_time)//60
     
 def calc_payment(user_id, session_id):
     data = select_all_data(user_id, session_id)
     start_time = data['start_time']
-    free_time = data['free_time']
+    free_time = data['free_period']
     payment_per_hour = data['payment_per_hour']
     return (((int(time.time()) - start_time)//60 - free_time)/60)*payment_per_hour
 
