4005283dc3a174b5369dfd1cabe2ba1d89c6f081
MashaSamoylova <mashanedyak@gmail.com>
2018-07-28 17:49:23 +0700

    asdf

--- db_manager.py
+++ db_manager.py
@@ -11,29 +11,31 @@ def prepare_db():
     c = db.cursor()
     c.execute(
             '''
-            create table if not exists PARKING (
-            user_id TEXT PRIMARY KEY,
+            create table PARKING (
+            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            user_id TEXT not null ,
             session_id TEXT not null,
             place TEXT,
             start_time TEXT,
             free_period INTEGER,
-            payment_per_hour INTEGER
+            payment_per_hour INTEGER,
+            stage_id TEXT,
+            closed INTEGER
           )
         ''')
     db.commit()
     db.close()
-    return 0
 
 
-def insert(user_id="", session_id="", place="", start_time="", free_period="", payment_per_hour=""):
+def insert(user_id="", session_id="", place="", start_time="", free_period="", payment_per_hour="", closed_status=0):
     if user_id == "":
         raise Exception("user_id is null")
 
     db = get_db()
     c = db.cursor()
     c.execute('''
-        INSERT INTO PARKING (user_id, session_id, place, start_time, free_period, payment_per_hour)
-        VALUES (?,?,?,?, ?,?)
+        INSERT INTO PARKING (user_id, session_id, place, start_time, free_period, payment_per_hour, stage_id, closed)
+        VALUES (?,?,?,?,?,?, NULL, ?)
     ''', 
     (
         user_id,
@@ -42,79 +44,89 @@ def insert(user_id="", session_id="", place="", start_time="", free_period="", p
         start_time,
         free_period,
         payment_per_hour,
+        closed_status
         ))
     db.commit()
     db.close()
-    
-    return 0
 
-def update_place(user_id, session_id, place):
+def update_place(place, id_s):
     db = get_db()
     c = db.cursor()
     c.execute('''
     UPDATE PARKING
     SET place = ?
-    WHERE session_id=? AND user_id=?
-    ''', (place, session_id, user_id))
+    WHERE id=?
+    ''', (place, _id))
 
     db.commit()
     db.close()
-    return 0
 
-def update_start_time(user_id, session_id, start_time):
+def update_start_time(start_time, id_s):
     db = get_db()
     c = db.cursor()
     c.execute('''
     UPDATE PARKING
     SET start_time = ?
-    WHERE session_id=? AND user_id=?
-    ''', (start_time, session_id, user_id))
+    WHERE id=?
+    ''', (start_time, id_s))
 
     db.commit()
     db.close()
     return 0
 
-def update_free_period(user_id, session_id, free_period):
+def update_free_period(free_period, id_s):
     db = get_db()
     c = db.cursor()
     c.execute('''
     UPDATE PARKING
     SET free_period = ?
-    WHERE session_id=? AND user_id=?;
-    ''', (free_period, session_id, user_id))
+    WHERE id=?;
+    ''', (free_period, id_s ))
 
     db.commit()
     db.close()
-    return 0
 
-def update_payment_per_hour(user_id, session_id, payment_per_hour):
+def update_payment_per_hour(payment_per_hour, id_s):
     db = get_db()
     c = db.cursor()
     c.execute('''
     UPDATE PARKING
     SET payment_per_hour = ?
-    WHERE session_id=? AND user_id=?;
-    ''', (payment_per_hour, session_id, user_id))
-
+    WHERE id=?
+    ''', (payment_per_hour, id_s))
     db.commit()
     db.close()
-    return 0
+
+def update_close_status(closed_status, id_s):
+    db = get_db()
+    c = db.cursor()
+    c.execute('''
+    UPDATE PARKING
+    SET closed = 1
+    WHERE id=?;
+    ''', (closed_status, id_s))
+    db.commit()
+    db.close()
 
 def select_all_data(user_id, session_id):
     db = get_db()
     c = db.cursor()
     c.execute('''
-    SELECT place, start_time, free_period, payment_per_hour from PARKING
-    WHERE session_id=? AND user_id=?;
+    SELECT id, user_id, session_id, place, start_time, free_period, payment_per_hour, stage_id, closed from PARKING
+    WHERE  id = (SELECT MAX(id) FROM PARKING WHERE session_id=? AND user_id=? and closed=0);
     ''', (session_id, user_id))
 
     data = c.fetchall()[0]
 
     return {
-        'place': data[0],
-        'start_time': data[1],
-        'free_period': data[2],
-        'payment_per_hour': data[3],
+        'id': data[0],
+        'user_id': data[1],
+        'session_id': data[2]
+        'place': data[3],
+        'start_time': data[4],
+        'free_period': data[5],
+        'payment_per_hour': data[6],
+        'stage_id': data[7],
+        'closed': data[8]
         }
-
 
