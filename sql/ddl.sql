e5f28cdc5b50504d84425e7401d5d9957d74e431
Nikolai Shtykov <stykov@gmail.com>
2018-07-28 15:32:09 +0500

    updated sql: added stage, id and closed columns

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,8 +1,13 @@
 create table PARKING (
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   user_id TEXT not null ,
   session_id TEXT not null,
   place TEXT,
   start_time TEXT,
   free_period INTEGER,
-  payment_per_hour INTEGER
+  payment_per_hour INTEGER,
+  stage_id TEXT,
+  closed INTEGER
 );
+
+DROP table PARKING
