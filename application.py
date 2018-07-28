4e09cb36c200a09497933423d1da4dd4e9fa29db
MashaSamoylova <mashanedyak@gmail.com>
2018-07-28 08:32:15 +0000

    неработающий

--- application.py
+++ application.py
@@ -1,14 +1,17 @@
 from flask import Flask, request
+from aiohttp import web
+from aioalice import Dispatcher, get_new_configured_app
 import logging
 
 application = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
+dp = Dispatcher()
 
 @application.route("/", methods=['POST'])
-def main():
-    logging.info('Request: %r', request.json)
-    return "HELLO"
+@dp.request_handler()
+async def handle_all_requests(alice_request):
+    return alice_request.response('Привет этому миру!')
 
 if __name__=="__main__":
-    application.run()
+    application.run(port='5000')
